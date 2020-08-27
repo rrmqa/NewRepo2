f9f8cb068e839c207e1fd881f7a9ac074bbef768
sdet2020 <cybertek.b20@gmail.com>
2020-08-26 23:30:33 -0500

    Birds & YouTube

---
+++ src/day02_SeleniumPractices/BirdsPractice.java
@@ -0,0 +1,30 @@
+package day02_SeleniumPractices;
+
+public class BirdsPractice {
+    /*
+    WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        String[] data = { "red birds", "yellow birds", "colorful butterflies"}; // SINGING BIRDS BEAUTIFULLY
+        String[] title = new String[3];
+        ArrayList<String> URL = new ArrayList<String>();
+
+
+        int i=0;
+        for (String each : data) {
+            driver.get("https://google.com");
+            driver.findElement(By.name("q")).sendKeys(each + Keys.ENTER);
+            driver.findElement(By.partialLinkText(each)).click();
+            //Thread.sleep(1000);
+            title[i] = driver.getTitle();
+            URL.add(driver.getCurrentUrl());
+
+            i++;
+        }
+        System.out.println(Arrays.toString(title));
+        System.out.println(URL);
+
+
+        driver.close();
+     */
+}
---
+++ src/day02_SeleniumPractices/YouTubePractice.java
@@ -0,0 +1,13 @@
+package day02_SeleniumPractices;
+
+public class YouTubePractice {
+
+    /*
+    WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://youtube.com");
+        driver.findElement(By.tagName("input")).sendKeys("SINGING BIRDS"+ Keys.ENTER);
+        driver.findElement(By.partialLinkText("SINGING BIRDS")).click();
+     */
+}
